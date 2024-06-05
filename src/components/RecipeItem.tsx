fea995f485e904074d726d7959879bf6f5c063ef
Moggach <acunnane13@gmail.com>
2024-06-05 20:38:37 +0100

    replace inline svgs with imports

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import isBookMarkedIcon from '../images/is-bookmarked-icon.svg'
+import isNotBookMarkedIcon from '../images/is-not-bookmarked-icon.svg'
 
 interface RecipeItemProps {
   recipe: Recipe;
@@ -16,7 +18,7 @@ export interface Recipe {
 
 const RecipeItem: React.FC<RecipeItemProps> = ({ recipe, onToggleBookmark, isBookmarked, variant = 'main' }) => {
   const containerClass = variant === 'main' ? 'w-[166px]' : 'w-[352px] md:w-[80%] flex items-center justify-between mx-auto';
-  const titleClass = variant === 'main' ? 'text-md font-medium text-textBlack' : 'text-md font-medium text-textWhite';
+  const titleClass = variant === 'main' ? 'text-md font-medium text-textBlack max-w-[70%]' : 'text-md font-medium text-textWhite';
   const imageWrapper = variant === 'main' ? '' : 'flex items-center gap-md';
   const imageClass = variant === 'main' ? 'rounded-xl mb-xs max-h-[166px] object-cover' : 'rounded-xl max-w-[54px] max-h-[54px] md:max-h-[120px] md:max-w-[120px] object-cover';
 
@@ -24,22 +26,16 @@ const RecipeItem: React.FC<RecipeItemProps> = ({ recipe, onToggleBookmark, isBoo
     <li className={containerClass} key={recipe.id}>
       <div className={imageWrapper}>
         {recipe.image && recipe.image.length > 0 && (
-          <img className={imageClass} src={recipe.image[0].url} alt={recipe.image[0].title} width="200" />
+          <img className={imageClass} src={recipe.image[0].url} alt={recipe.image[0].title} width="100%" />
         )}
         <div className="flex justify-between align-top">
           <h3 className={titleClass}>{recipe.title}</h3>
           {variant === 'main' && (
-            <button onClick={() => onToggleBookmark(recipe.id)}>
+            <button className="self-start" onClick={() => onToggleBookmark(recipe.id)}>
               {isBookmarked ? (
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-                  <circle cx="12.5" cy="12.5" r="12.5" fill="#404040" />
-                  <path d="M15.5517 7.765C16.1519 7.83484 16.5923 8.35264 16.5923 8.95721V17.4107L12.5 15.3646L8.40775 17.4107V8.95721C8.40775 8.35264 8.84753 7.83484 9.44827 7.765C11.476 7.52963 13.5241 7.52963 15.5517 7.765Z" stroke="#FAFAFA" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+              <img src={isBookMarkedIcon} alt="BookMark" />
               ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-                  <circle cx="12.5" cy="12.5" r="12.5" fill="#FFFFFF" stroke="#404040" />
-                  <path d="M15.5517 7.765C16.1519 7.83484 16.5923 8.35264 16.5923 8.95721V17.4107L12.5 15.3646L8.40775 17.4107V8.95721C8.40775 8.35264 8.84753 7.83484 9.44827 7.765C11.476 7.52963 13.5241 7.52963 15.5517 7.765Z" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
+                 <img src={isNotBookMarkedIcon} alt="Remove BookMark" />
               )}
             </button>
           )}
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RecipeItem } from './components/RecipeItem';
 import { Recipe } from './components/RecipeItem';
+import bookmarkIcon from './images/bookmark-icon.svg'
+import closeIcon from './images/close-icon.svg';
 
 const App: React.FC = () => {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -54,64 +56,61 @@ const App: React.FC = () => {
 
   const isBookmarked = (id: number): boolean => bookmarkedRecipes.includes(id);
 
-  return (
-    <main className="bg-backgroundWhite font-inter relative">
-      <div className='w-[352px] md:w-[80%] mx-auto flex items-baseline justify-between'>
-        <h1 className="px-md uppercase text-lg tracking-[0.4375em] font-semibold text-textHeading leading-header pt-xl pb-lg flex-1 flex justify-center">Recipebox</h1>
-        <button className="ml-auto relative" onClick={togglePopup}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="18" viewBox="0 0 14 18" fill="none">
-            <path d="M11.8519 1.41466C12.7927 1.53021 13.4829 2.38694 13.4829 3.3872V17.3738L7.06846 13.9884L0.654053 17.3738V3.3872C0.654053 2.38694 1.34339 1.53021 2.28502 1.41466C5.46331 1.02523 8.67361 1.02523 11.8519 1.41466Z" fill="#404040" stroke="#404040" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-          {bookmarkedRecipes.length > 0 && (
-            <span className="absolute top-[-5px] right-[-5px] inline-flex items-center justify-center text-sm font-bold bg-buttonRed text-textWhite rounded-full w-[13px] h-[13px]">
-              {bookmarkedRecipes.length}
-            </span>
-          )}
-        </button>
-      </div>
-      {isLoading && <p>Loading...</p>}
-      {hasError && <p>Oops! Error occurred while fetching recipes. Report this as a bug 🤖</p>}
 
-      <ul className="flex flex-wrap justify-center gap-md w-[352px] md:w-[80%] mx-auto">
-        {recipes.map((recipe: Recipe) => (
-          <RecipeItem
-            key={recipe.id}
-            recipe={recipe}
-            onToggleBookmark={toggleBookmark}
-            isBookmarked={isBookmarked(recipe.id)}
-          />
-        ))}
-      </ul>
-
-      {isPopupVisible && (
-        <>
-          <div className="fixed inset-0 bg-backgroundGray pointer-events-none z-40"></div>
-          <div className="fixed bottom-0 left-0 w-full bg-backgroundBlack pb-lg rounded-t-3xl z-50">
-            <div className="relative pt-xl">
-              <button onClick={togglePopup} className="absolute top-4 right-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
-                  <path d="M1.5 13L13.5 1M1.5 1L13.5 13" stroke="#FAFAFA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </button>
+    return (
+      <main className="bg-backgroundWhite font-inter relative">
+        <div className='w-[352px] md:w-[80%] mx-auto flex items-baseline justify-between'>
+          <h1 className="px-md uppercase text-lg tracking-[0.4375em] font-semibold text-textHeading leading-header pt-xl pb-lg flex-1 flex justify-center">Recipebox</h1>
+          <button className="ml-auto relative" onClick={togglePopup}>
+            <img src={bookmarkIcon} alt="Bookmark" />
+            {bookmarkedRecipes.length > 0 && (
+              <span className="absolute top-[-5px] right-[-5px] inline-flex items-center justify-center text-sm font-bold bg-buttonRed text-textWhite rounded-full w-[13px] h-[13px]">
+                {bookmarkedRecipes.length}
+              </span>
+            )}
+          </button>
+        </div>
+        {isLoading && <p>Loading...</p>}
+        {hasError && <p>Oops! Error occurred while fetching recipes. Report this as a bug 🤖</p>}
+    
+        <ul className="flex flex-wrap justify-center gap-md w-[352px] md:w-[80%] mx-auto">
+          {recipes.map((recipe: Recipe) => (
+            <RecipeItem
+              key={recipe.id}
+              recipe={recipe}
+              onToggleBookmark={toggleBookmark}
+              isBookmarked={isBookmarked(recipe.id)}
+            />
+          ))}
+        </ul>
+    
+        {isPopupVisible && (
+          <>
+            <div className="fixed inset-0 bg-backgroundGray pointer-events-none z-40"></div>
+            <div className="fixed bottom-0 left-0 w-full bg-backgroundBlack pb-lg rounded-t-3xl z-50">
+              <div className="relative pt-xl">
+                <button onClick={togglePopup} className="absolute top-4 right-4">
+                  <img src={closeIcon} alt="Close"/>
+                </button>
+              </div>
+              <ul className="flex flex-col gap-md">
+                {recipes
+                  .filter((recipe: any) => bookmarkedRecipes.includes(recipe.id))
+                  .map((recipe: any) => (
+                    <RecipeItem
+                      key={recipe.id}
+                      recipe={recipe}
+                      onToggleBookmark={toggleBookmark}
+                      isBookmarked={isBookmarked(recipe.id)}
+                      variant="popup"
+                    />
+                  ))}
+              </ul>
             </div>
-            <ul className="flex flex-col gap-md">
-              {recipes
-                .filter((recipe: any) => bookmarkedRecipes.includes(recipe.id))
-                .map((recipe: any) => (
-                  <RecipeItem
-                    key={recipe.id}
-                    recipe={recipe}
-                    onToggleBookmark={toggleBookmark}
-                    isBookmarked={isBookmarked(recipe.id)}
-                    variant="popup"
-                  />
-                ))}
-            </ul>
-          </div>
-        </>
-      )}
-    </main>
-  );
+          </>
+        )}
+      </main>
+    );
 };
 
 createRoot(document.getElementById('root')!).render(<App />);
